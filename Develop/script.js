3efa4307a62ac323c8423291e09022c2f07f9b08
Toliver Crisp <92277632+tolivercrisp@users.noreply.github.com>
2023-01-31 14:04:48 -0500

    Succesfully parses the users input and checks for valid range input

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
 // Function -- Produces a random number
 function randomInteger(min, max) {
   if (!max) {
@@ -22,18 +19,40 @@ function ranItem(list) {
 // Main function -- Generates the actual password
 function generatePassword() {
 
+    // Array (empty) -- Stores the character types that the user wants to include
+    var passwordElementStorage = []
+
+    // String (empty) -- Stores the finalized password to be display once it is generated at the end of the function
+    var finalPassword = ""
+
   // Arrays for character types
   const numberArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
   const upperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
   const lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
   const symbolArray = ["!", "@", "#", "$", "%", "^", "&", "*", "?", "-"]
 
-
   // Window confirmation message that retrieves a boolean value from
   var numberConfirm = window.confirm("Would you like to include NUMBERS in your password?")
   var upperConfirm = window.confirm("Would you like to include UPPERCASE LETTERS in your password?")
   var lowerConfirm = window.confirm("Would you like to include LOWERCASE LETTERS in your password?")
   var symbolConfirm = window.confirm("Would you like to include SYMBOLS in your password?")
+
+// The Users response to the question is stored in the variable 'userAnswer'
+  var userAnswer = window.prompt("How many characters would you like in your generated password? (8-128)")
+
+// Changes the data type of the user's answer from 'string' to 'number' to use later
+  var userLengthChoice = parseInt(userAnswer)
+
+//   Makes sure that the requested length is in the valid range, and if not, throws error message to client
+  if (userLengthChoice < 8 || userLengthChoice > 128) {
+    window.alert("Requested password length must be between 8 and 128.")
+  }
+
+
+
+
+
+
 
 
 
